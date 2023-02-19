9f009be65381321e7a722e51f4f340a0a494f4a9
Daniele <83521498+danymarsico@users.noreply.github.com>
2023-02-18 22:08:45 -0500

    create function that picks computer's choice

--- rps.js
+++ rps.js
@@ -1,3 +1,14 @@
+/*The following function will pick the computer's choice*/
 function getComputerChoice() {
-    
+let choice = ["rock", "paper", "scissors"],
+ pickedChoice = choice[Math.floor(Math.random() * choice.length)];
+ console.log(pickedChoice);
 }
+
+/*This following function will play a round*/
+function playRound(playerSelection, computerSelection) {
+
+}
+
+
+console.log(getComputerChoice());
